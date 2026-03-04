2774e1123278a846563d536272dfb92d6779daab
Semyon Yanichev <janchevsem@mail.ru>
2026-03-04 12:06:43 +0400

    feat: user auth

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register'])
         ->name('auth.register');
+    Route::post('/login', [AuthController::class, 'login'])
+        ->name('auth.login');
+    Route::post('/logout', [AuthController::class, 'logout'])
+        ->name('auth.logout');
 });
